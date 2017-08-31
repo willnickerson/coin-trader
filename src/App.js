ac6a97f77ea02b08f70c4860c4dc44afdb17fe3d
willnickerson <wnickers@willamette.edu>
2017-08-30 19:32:47 -0700

    initial commit, boiler plate and router set up

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import './App.css';
+import { Switch, Route, Redirect, NavLink } from 'react-router-dom';
+import Home from './Home';
+import About from './About';
+import Account from './Account';
+import Trade from './Trade';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h2>Coin Trader</h2>
+          <nav>
+            <NavLink to='/'>Home</NavLink>
+            <NavLink to='/trade'>Trade</NavLink>
+            <NavLink to='/account'>Account</NavLink>
+            <NavLink to='/about'>About</NavLink>           
+          </nav>
+        </div>
+
+        <Switch>
+          <Route path='/' exact component={Home}/>
+          <Route path='/trade' component={Trade}/>
+          <Route path='/account' component={Account}/>
+          <Route path='/about' component={About}/>
+          <Redirect to='/'/>
+        </Switch>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/Trade.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Trade extends Component {
+  render() {
+    return (
+      <div>
+        Trade
+      </div>
+    );
+  }
+}
+
+export default Trade;
