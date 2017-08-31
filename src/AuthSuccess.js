f43010efa92d056a7f5c65b3d797622eef682a02
willnickerson <wnickers@willamette.edu>
2017-08-30 21:34:41 -0700

    succesfully recieiving auth token

--- src/AuthSuccess.js
+++ src/AuthSuccess.js
@@ -1,6 +1,23 @@
 import React, { Component } from 'react';
+import { getToken } from './api';
+import qs from 'query-string';
 
 class AuthSuccess extends Component {
+  state = {
+    token: null
+  };
+
+  componentDidMount() {
+    const query = qs.parse(this.props.location.search);
+    debugger;
+    if(query.code) {
+      getToken(query.code)
+        .then(data => {
+          debugger;
+        })
+    }
+  }
+  
   render() {
     return (
       <div>
--- src/api.js
+++ src/api.js
@@ -1,7 +1,21 @@
-import { CLIENT_ID } from './consts';
+import { CLIENT_ID, CLIENT_SECRET, REDIRECT_URI } from './consts';
+
 
 const permissions = 'wallet:accounts:read,wallet:transactions:read,wallet:buys:create,wallet:sells:create';
 
 export const redirectUrl  = `https://www.coinbase.com/oauth/authorize?response_type=code&client_id=${CLIENT_ID}&scope=${permissions}`;
+
+const formTokenUrl = (code) => `https://api.coinbase.com/oauth/token?grant_type=authorization_code&code=${code}&client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&redirect_uri=${REDIRECT_URI}`;
+
+export const getToken = (code) => {
+  const url = formTokenUrl(code);
+  return fetch(url, { method: 'POST' })
+    .then(res => {
+      const data = res.json();
+      debugger;
+      return data;
+    })
+    .catch(err => err.message);
+}
 
 
